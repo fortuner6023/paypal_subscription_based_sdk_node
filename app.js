bc774acd43d8d092f1aa90a45aa8c432e017bacf
FORTUNER6023 <Naveen Saini>
2020-06-06 07:32:31 +0530

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const ejs = require('ejs');
 const paypal = require('paypal-rest-sdk');
+const port = 3000
 
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
-  'client_id': 'AaU8tQfmz1_MFDTKuf84yYERXvdDt2ZFJVrxhNW_49DazF4A_F0VBuKyV5_nntyEdZqUa5Oq9ZBj65GV',
-  'client_secret': 'EAZ8aFDU4lHHLy1bQqULYWqznf3dBknXZW3AH__zFC0bUs8AGUyR6RNbm-jHvqtikX7PsSqMO5vxuvKm'
-});
+  'client_id': 'AVu2VagBZvVqrvrJFRChYilbygYSYD_5i_UhOKgf12K3hwtmjSsNNJKWmd99sp_gqDnaN8zwJlrLmPv8',
+  'client_secret': 'EMxgeuRPmIp-XRlITFkfWrYiNNi9eM8dW2sw72e4QtxPCNnYFSPMIMTsc915Jfk_2XKrqPJxryf1wvqO'
+}); 
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) =>{console.log('hello from home page') 
+res.render('index')});
 
 app.post('/pay', (req, res) => {
   const create_payment_json = {
@@ -21,31 +23,32 @@ app.post('/pay', (req, res) => {
         "payment_method": "paypal"
     },
     "redirect_urls": {
-        "return_url": "http://localhost:3000/success",
-        "cancel_url": "http://localhost:3000/cancel"
+        "return_url": "http://3.12.198.193:3000/success",
+        "cancel_url": "http://3.12.198.193:3000/cancel"
     },
     "transactions": [{
         "item_list": {
             "items": [{
-                "name": "Red Sox Hat",
+                "name": "Subscribe for personal plan",
                 "sku": "001",
-                "price": "25.00",
+                "price": "20.00",
                 "currency": "USD",
                 "quantity": 1
             }]
         },
         "amount": {
             "currency": "USD",
-            "total": "25.00"
+            "total": "20.00"
         },
-        "description": "Hat for the best team ever"
+        "description": "Personal Plan"
     }]
 };
 
 paypal.payment.create(create_payment_json, function (error, payment) {
-  if (error) {
+  if (error) { 
       throw error;
   } else {
+    console.log('payment',payment)
       for(let i = 0;i < payment.links.length;i++){
         if(payment.links[i].rel === 'approval_url'){
           res.redirect(payment.links[i].href);
@@ -55,7 +58,7 @@ paypal.payment.create(create_payment_json, function (error, payment) {
 });
 
 });
-
+ 
 app.get('/success', (req, res) => {
   const payerId = req.query.PayerID;
   const paymentId = req.query.paymentId;
@@ -65,7 +68,7 @@ app.get('/success', (req, res) => {
     "transactions": [{
         "amount": {
             "currency": "USD",
-            "total": "25.00"
+            "total": "20.00"
         }
     }]
   };
@@ -83,4 +86,4 @@ app.get('/success', (req, res) => {
 
 app.get('/cancel', (req, res) => res.send('Cancelled'));
 
-app.listen(3000, () => console.log('Server Started'));
+app.listen(port || 3000, () => console.log('Server Started on port 3000'));
